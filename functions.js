1d1cf0d6f93e341bbd28b7b65683bef9cd3a6fb0
Nathan Masters <nathan.m@prestigegifting.co.uk>
2023-03-19 00:47:13 +0000

    first commit

---
+++ functions.js
@@ -0,0 +1,64 @@
+function addTwoNumbers() {
+  // Add two numbers together
+}
+
+function minusTwoNumbers() {
+  // Minus two numbers
+}
+
+function multiplyTwoNumbers() {
+  // Multiply two numbers
+}
+
+function devideTwoNumbers() {
+  // Devide two numbers
+}
+
+function evenOrOdd() {
+  // Given a number
+  // Work out if number is even or odd
+}
+
+function greeting() {
+  // Given a name
+  // Return "Hello, my name is [insert_name]"
+}
+
+function getFirstLetter() {
+  // Given a name
+  // Return the first letter of a name
+}
+
+function getInitials() {
+  // Given a full name
+  // Return the initials
+}
+
+function isThereASausage() {
+  // Given some text
+  // Return true if the text contains the word 'sausage'
+}
+
+function turnStringIntoNumber() {
+  // Given a number in the form of a string
+  // Return a number
+}
+
+function turnNumberIntoString() {
+  // Given a string of a number
+  // Return a string
+}
+
+module.exports = {
+  addTwoNumbers,
+  minusTwoNumbers,
+  multiplyTwoNumbers,
+  devideTwoNumbers,
+  evenOrOdd,
+  greeting,
+  getFirstLetter,
+  getInitials,
+  isThereASausage,
+  turnStringIntoNumber,
+  turnNumberIntoString,
+};
---
+++ functions.test.js
@@ -0,0 +1,60 @@
+const { number } = require('yargs');
+const {
+  addTwoNumbers,
+  minusTwoNumbers,
+  multiplyTwoNumbers,
+  devideTwoNumbers,
+  evenOrOdd,
+  greeting,
+  getFirstLetter,
+  getInitials,
+  isThereASausage,
+  turnStringIntoNumber,
+  turnNumberIntoString,
+} = require('./functions');
+
+describe('Basic number questions', () => {
+  test('Adds two numbers together', () => {
+    expect(addTwoNumbers(1, 2)).toBe(3);
+  });
+  test('Minuses two numbers together', () => {
+    expect(minusTwoNumbers(4, 2)).toBe(2);
+  });
+  test('Multiplies two numbers together', () => {
+    expect(multiplyTwoNumbers(4, 2)).toBe(8);
+  });
+  test('Devides two numbers together', () => {
+    expect(devideTwoNumbers(4, 2)).toBe(2);
+  });
+  test('Return true if number is even', () => {
+    expect(evenOrOdd(4)).toBe(true);
+  });
+  test('Return false if number is odd', () => {
+    expect(evenOrOdd(3)).toBe(false);
+  });
+});
+
+describe('Basic String questions', () => {
+  test('Return greeting', () => {
+    expect(greeting('Nathan')).toBe('Hello, my name is Nathan');
+  });
+  test('Return first letter of a name', () => {
+    expect(getFirstLetter('Nathan')).toBe('N');
+  });
+  test('Return initials of a full name', () => {
+    expect(getInitials('Nathan', 'Masters')).toBe('NM');
+  });
+  test('Return true if the text contains the word "sausage"', () => {
+    expect(
+      isThereASausage(
+        'I do not contain the word sausage, please look elsewhere.'
+      )
+    ).toBe(true);
+  });
+  test('Turn a string into a number', () => {
+    expect(turnStringIntoNumber('3')).toEqual(expect.any(Number));
+  });
+  test('Turn a number into a string', () => {
+    expect(turnNumberIntoString(3)).toEqual(expect.any(String));
+  });
+});
